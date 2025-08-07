b1e8fc11b20051c8500afcb09237299adec88f98
Santiago Hernandez <santiagooheernandez@gmail.com>
2025-08-06 23:30:39 -0400

    a

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
-st.title('Pyser')
+st.title("Pyser")
 
-st.file_uploader('Upload a file', type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+st.file_uploader(
+    "Upload a file",
+    type=None,
+    accept_multiple_files=False,
+    key=None,
+    help=None,
+    on_change=None,
+    args=None,
+    kwargs=None,
+    disabled=False,
+    label_visibility="visible",
+)
+
+print("File uploader component rendered successfully.")
